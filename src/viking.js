93f7967304b1eabfa0b378dcad4b93aa2cdbb713
Inês Almeida <ines_almeida_203@hotmail.com>
2023-04-04 23:21:14 +0100

    until iteration 3 but incomplete

--- src/viking.js
+++ src/viking.js
@@ -1,11 +1,53 @@
 // Soldier
-class Soldier {}
+class Soldier {
+    constructor(health, strength) {
+        this.health = health;
+        this.strength = strength;
+    }
+    attack() {
+        return this.strength
+    }
+    receiveDamage(theDamage) {
+        this.health = this.health - theDamage 
+    }
+}
 
 // Viking
-class Viking {}
+class Viking extends Soldier {
+    constructor(name, health, strength) {
+        super(health, strength);
+        this.name;
+    }
+    /*attack() {
+        return this.strength
+    }*/ //put the methods only when have changes? or its a good practice to put always? since inheritance is for attributes and methods
+    receiveDamage(theDamage) {
+        this.health = this.health - theDamage 
+         if (this.health > 0) {
+            return `${this.name} has received ${this.health} points of damage`
+         } else {
+             return `${this.name} has died in act of combat`
+        }
+    }
+    battleCry() {
+        return 'Odin Owns You All!'
+    }
+}
 
 // Saxon
-class Saxon {}
+class Saxon extends Soldier { 
+    constructor(health, strength) {
+        super(health,strength);
+    }
+receiveDamage(theDamage) {
+    this.health = this.health - theDamage 
+     if (this.health > 0) {
+         return `A Saxon has received ${this.health} points of damage`
+     } else {
+         return 'A Saxon has died in combat'
+        }
+    }
+}
 
 // War
 class War {}
