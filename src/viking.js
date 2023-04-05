7b3504116a551fa051e17e5bfa49e558dcfdd7ef
Inês Almeida <ines_almeida_203@hotmail.com>
2023-04-05 23:12:00 +0100

    iteration 4 incomplete

--- src/viking.js
+++ src/viking.js
@@ -7,8 +7,8 @@ class Soldier {
     attack() {
         return this.strength
     }
-    receiveDamage(theDamage) {
-        this.health = this.health - theDamage 
+    receiveDamage(damage) {
+        this.health = this.health - damage 
     }
 }
 
@@ -17,15 +17,14 @@ class Viking extends Soldier {
     constructor(name, health, strength) {
         super(health, strength);
         this.name = name;
-
     }
     /*attack() {
         return this.strength
     }*/ //put the methods only when have changes? or its a good practice to put always? since inheritance is for attributes and methods
-    receiveDamage(theDamage) {
-        this.health = this.health - theDamage 
+    receiveDamage(damage) {
+        this.health = this.health - damage 
          if (this.health > 0) {
-            return `${this.name} has received ${theDamage} points of damage`
+            return `${this.name} has received ${damage} points of damage`
          } else {
              return `${this.name} has died in act of combat`
         }
@@ -40,10 +39,10 @@ class Saxon extends Soldier {
     constructor(health, strength) {
         super(health,strength);
     }
-receiveDamage(theDamage) {
-    this.health = this.health - theDamage 
+receiveDamage(damage) {
+    this.health = this.health - damage 
      if (this.health > 0) {
-         return `A Saxon has received ${theDamage} points of damage`
+         return `A Saxon has received ${damage} points of damage`
      } else {
          return 'A Saxon has died in combat'
         }
@@ -51,4 +50,41 @@ receiveDamage(theDamage) {
 }
 
 // War
-class War {}
+class War {
+    constructor() {
+    }
+    vikingArmy = []
+    saxonArmy = []
+addViking(viking) {
+    this.vikingArmy.unshift(viking) //why put this.??
+}
+addSaxon(saxon) {
+    this.saxonArmy.unshift(saxon)
+}
+vikingAttack() {
+    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+    let randomViking = Math.floor(Math.random() * this.vikingArmy.length)
+//Saxon receiveDamage() equal to the strength of a Viking should remove dead Saxons from the army
+    this.receiveDamage(randomSaxon) = this.attack(randomViking)
+    this.saxonArmy.shift()
+    //return `Saxon has ${this.damage.saxon} of damage and Viking has ${this.strength.viking} of strength`
+}
+saxonAttack() {
+    let randomSaxon = Math.floor(Math.random() * this.saxonArmy.length)
+    let randomViking = Math.floor(Math.random() * this.vikingArmy.length)
+    //Viking receiveDamage() equal to the strength of a Saxon
+    this.saxonArmy.shift()
+    //return `Viking has ${this.damage.viking} of damage and Saxon has ${this.strength.saxon} of strength`
+}
+showStatus() {
+    if(this.saxonArmy.length === 0) {
+        return 'Vikings have won the war of the century!'
+    }
+    if(this.vikingArmy.length === 0) {
+       return 'Saxons have fought for their lives and survived another day...'
+    }
+    if(this.saxonArmy.length === 1 && this.vikingArmy.length === 1) {
+        return 'Vikings and Saxons are still in the thick of battle.'
+    }
+}
+}
